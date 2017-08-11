2583d0fe1afa10e2de996e7293fb47d3df84aa4b
L, Shijithlal <Shijithlal_L@intuit.com>
2017-08-11 15:56:48 +0530

    Initial Sprint boot application

---
+++ src/main/java/com/shijith/sms/SmsHandlerApplication.java
@@ -0,0 +1,12 @@
+package com.shijith.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsHandlerApplication.class, args);
+	}
+}
