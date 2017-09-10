7703abe333016f578156398466ce5c5ff64ef953
L, Shijithlal <Shijithlal_L@intuit.com>
2017-09-11 01:25:02 +0530

    Add index to username

--- db/sqldump.sql
+++ db/sqldump.sql
@@ -26,7 +26,8 @@ CREATE TABLE `account` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `auth_id` varchar(40) DEFAULT NULL,
   `username` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -76,4 +77,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-08-12 15:30:06
+-- Dump completed on 2017-09-11  1:24:29
